3b251f1e0b189c8d8b89eee3e1e813a85546dac0
logan.gauthier@metova.com <logan.gauthier@metova.com>
2014-05-29 17:31:42 -0500

    Added a project to implement unit tests.

---
+++ ThunderCats/ThunderCatsTests/NSStringTests.m
@@ -0,0 +1,17 @@
+//
+//  NSStringTests.m
+//  ThunderCats
+//
+//  Created by Metova on 5/29/14.
+//  Copyright (c) 2014 Metova. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSStringTests : XCTestCase
+
+@end
+
+@implementation NSStringTests
+
+@end
