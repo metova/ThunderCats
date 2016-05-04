433a1d0d052459a7f5b67fc8b4a36b6481c946c0
Nick Griffith <nick.griffith@metova.com>
2016-05-04 18:03:52 -0500

    removing UIDevice deprecated methods

--- Categories/UIDevice+TCAdditions.h
+++ Categories/UIDevice+TCAdditions.h
@@ -30,31 +30,12 @@
 #import <UIKit/UIKit.h>
 #import "TCNullabilityMacros.h"
 
-FOUNDATION_EXPORT NSString * __tc_nonnull const kVersionStringIOS6 __deprecated_msg("This constant is missing the TC prefix. It will be removed in 2.0. Use TCIOSVersionStringIOS6 instead.");
-FOUNDATION_EXPORT NSString * __tc_nonnull const kVersionStringIOS7 __deprecated_msg("This constant is missing the TC prefix. It will be removed in 2.0. Use TCIOSVersionStringIOS7 instead.");
-FOUNDATION_EXPORT NSString * __tc_nonnull const kVersionStringIOS8 __deprecated_msg("This constant is missing the TC prefix. It will be removed in 2.0. Use TCIOSVersionStringIOS8 instead.");
-
 FOUNDATION_EXPORT NSString * __tc_nonnull const TCIOSVersionStringIOS6;
 FOUNDATION_EXPORT NSString * __tc_nonnull const TCIOSVersionStringIOS7;
 FOUNDATION_EXPORT NSString * __tc_nonnull const TCIOSVersionStringIOS8;
 
 
 @interface UIDevice (TCAdditions)
-
-/**
- *  Returns a Boolean value that indicates whether the screen has a height of 480.
- *
- *  @return YES if the height of the screen is 480, otherwise NO.
- */
-+ (BOOL)tc_isScreenHeight480 __deprecated_msg("We were silly to think that this was a good idea. It will be removed in 2.0.");
-
-/**
- *  Returns a Boolean value that indicates whether the screen has a height of 568.
- *
- *  @return YES if the height of the screen is 568, otherwise NO.
- */
-+ (BOOL)tc_isScreenHeight568 __deprecated_msg("We were silly to think that this was a good idea. It will be removed in 2.0.");
-
 
 /**
  *  Returns the current size of the screen. The screen's dimensions are inversed if the device is in landscape.
@@ -92,14 +73,6 @@ FOUNDATION_EXPORT NSString * __tc_nonnull const TCIOSVersionStringIOS8;
 + (CGFloat)tc_statusBarHeight;
 
 /**
- *  Returns the height of the navigation bar. This is a method which will likely introduce bugs. Just don't use it.
- *
- *  @return 44.0 if the device is in portrait mode. 32.0 if the device is in landscape.
- */
-+ (CGFloat)tc_navigationBarHeight __deprecated_msg("We were silly to think that this was a good idea. It will be removed in 2.0.");
-
-
-/**
  *  Returns a Boolean value that indicates whether the device is an iPad.
  *
  *  @return YES if the device is an iPad, NO otherwise.
@@ -112,7 +85,6 @@ FOUNDATION_EXPORT NSString * __tc_nonnull const TCIOSVersionStringIOS8;
  *  @return YES if the device is an iPhone, NO otherwise.
  */
 + (BOOL)tc_isPhone;
-
 
 /**
  *  Returns a Boolean value that indicates whether the device's iOS version is equal to the supplied version string.
